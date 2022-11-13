c92bf525db6d1a5d2c6f7bd3e32bf835a912b5f4
Hojin Yun <hojin.yun71@gmail.com>
2022-11-13 00:13:26 -0500

    edit StringComparator access modifier

--- Homework18/src/StringComparator.java
+++ Homework18/src/StringComparator.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 
-class StringComparator implements Comparator<String> {
+public class StringComparator implements Comparator<String> {
 
     @Override
     public int compare(String s1, String s2) {
